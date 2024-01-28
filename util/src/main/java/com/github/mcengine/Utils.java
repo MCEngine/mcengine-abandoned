bc65370e688c005c1a406e59919100aad1db6162
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-28 19:49:25 +0700

    add readPropertiesFile

--- util/src/main/java/com/github/mcengine/Utils.java
+++ util/src/main/java/com/github/mcengine/Utils.java
@@ -1,5 +1,19 @@
 package com.github.mcengine;
 
 public class Utils {
-    
+    public static Properties readPropertiesFile(String filePath) {
+        // Create a Properties object
+        Properties properties = new Properties();
+
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+            // Load the properties from the file
+            properties.load(fileInputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Return the Properties object
+        return properties;
+    }
 }
