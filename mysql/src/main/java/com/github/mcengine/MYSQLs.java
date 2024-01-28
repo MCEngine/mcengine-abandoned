826c97e803f1e170d239404ca5b46b49fc27aa16
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-28 20:01:20 +0700

    add mysql initialize, close --> connection

--- mysql/src/main/java/com/github/mcengine/MYSQLs.java
+++ mysql/src/main/java/com/github/mcengine/MYSQLs.java
@@ -1,10 +1,12 @@
 package com.github.mcengine;
 
+import java.sql.*;
 import java.util.Properties;
 
 public class MYSQLs {
     private static String path;
     private static Properties properties; // Delay initialization
+    private static Connection connection;
 
     public static String getPath() {
         return path;
@@ -18,5 +20,24 @@ public class MYSQLs {
     // If you need to access properties from outside, consider adding a getter for it
     public static Properties getProperties() {
         return properties;
+    }
+
+    // Initialize the connection
+    public static void initializeConnection() throws SQLException {
+        String dbUrl = properties.getProperty("url");
+        String dbUsername = properties.getProperty("username");
+        String dbPassword = properties.getProperty("password");
+        String dbName = properties.getProperty("dbname");
+        String dbPort = properties.getProperty("port");
+
+        String fullDbUrl = dbUrl + ':' + dbPort + '/' + dbName;
+        connection = DriverManager.getConnection(fullDbUrl, dbUsername, dbPassword);
+    }
+
+    // Close the connection
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
     }
 }
