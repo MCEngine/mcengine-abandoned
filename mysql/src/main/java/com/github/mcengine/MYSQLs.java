7383debc05e1391b94bc01a71eccb70fce1524b9
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-28 19:52:54 +0700

    Create path get/set

--- mysql/src/main/java/com/github/mcengine/MYSQLs.java
+++ mysql/src/main/java/com/github/mcengine/MYSQLs.java
@@ -1,5 +1,13 @@
 package com.github.mcengine;
 
 public class MYSQLs {
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
     
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
