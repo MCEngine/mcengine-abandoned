3db9a4569659228a0a5eb943d31f67c9bba04d42
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-29 12:07:21 +0700

    add func checkFile

--- util/src/main/java/com/github/mcengine/Check.java
+++ util/src/main/java/com/github/mcengine/Check.java
@@ -1,6 +1,7 @@
 package com.github.mcengine;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Check {
     // Check if directory exits
@@ -13,6 +14,28 @@ public class Check {
                 System.out.println("Directories created: " + dir.getAbsolutePath());
             } else {
                 System.out.println("Failed to create directories.");
+            }
+        }
+    }
+    
+    // Check if the file exists
+    public static void checkFile(File file) {
+        if (file.exists()) {
+            System.out.println("The file already exists: " + file.getAbsolutePath());
+        } else {
+            try {
+                // Create parent directories if they don't exist
+                File fileParent = file.getParentFile();
+                checkDir(fileParent);
+
+                // Create the file
+                if (file.createNewFile()) {
+                    System.out.println("File created: " + file.getAbsolutePath());
+                } else {
+                    System.out.println("File creation failed. The file may already exist.");
+                }
+            } catch (IOException e) {
+                System.err.println("Error creating the file: " + e.getMessage());
             }
         }
     }
