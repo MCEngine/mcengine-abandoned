ac7e0f76d43085db652b9c4c194fd7fa2694c56b
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-29 12:06:41 +0700

    add func checkDir

--- util/src/main/java/com/github/mcengine/Check.java
+++ util/src/main/java/com/github/mcengine/Check.java
@@ -1,5 +1,19 @@
 package com.github.mcengine;
 
+import java.io.File;
+
 public class Check {
-    
+    // Check if directory exits
+    public static void checkDir(File dir) {
+        if (dir.exists()) {
+            System.out.println("Directories exists: " + dir.getAbsolutePath());
+        } else {
+            // Create directories recursively if they don't exist
+            if (dir.mkdirs()) {
+                System.out.println("Directories created: " + dir.getAbsolutePath());
+            } else {
+                System.out.println("Failed to create directories.");
+            }
+        }
+    }
 }
