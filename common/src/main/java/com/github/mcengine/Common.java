cd55344c2c5875b4e094d5970d1d8218a39eab7d
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-29 15:52:19 +0700

    Change code locate

--- common/src/main/java/com/github/mcengine/Common.java
+++ common/src/main/java/com/github/mcengine/Common.java
@@ -1,5 +1,56 @@
 package com.github.mcengine;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Common {
+    // Check if directory exits
+    public static void checkDir(File dir) {
+        if (dir.exists()) {
+            System.out.println("Directories exists: " + dir.getAbsolutePath());
+        } else {
+            // Create directories recursively if they don't exist
+            if (dir.mkdirs()) {
+                System.out.println("Directories created: " + dir.getAbsolutePath());
+            } else {
+                System.out.println("Failed to create directories.");
+            }
+        }
+    }
     
+    // Check if the file exists
+    public static void checkFile(File file) {
+        if (file.exists()) {
+            System.out.println("The file already exists: " + file.getAbsolutePath());
+        } else {
+            try {
+                // Create parent directories if they don't exist
+                File fileParent = file.getParentFile();
+                checkDir(fileParent);
+
+                // Create the file
+                if (file.createNewFile()) {
+                    System.out.println("File created: " + file.getAbsolutePath());
+                } else {
+                    System.out.println("File creation failed. The file may already exist.");
+                }
+            } catch (IOException e) {
+                System.err.println("Error creating the file: " + e.getMessage());
+            }
+        }
+    }
+
+    // Check if the Database Table exists
+    public static void checkDBTable(String[] querys) throws SQLException {
+        try {
+            MYSQLs.initializeConnection();
+            for (String query : querys) {
+                MYSQLs.executeQuery(query);
+            }
+        } finally {
+            // Ensure the connection is closed even if an exception occurs
+            MYSQLs.closeConnection();
+        }
+    }
 }
