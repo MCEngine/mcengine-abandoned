13aa74a3d1f4931ed5ae3237182fb3ef3d6a983e
owen <56456776+JetsadaWijit@users.noreply.github.com>
2024-03-10 07:27:00 +0000

    add func [ "getGitHubLatestReleaseTag" ]

--- util/src/main/java/com/github/mcengine/Util.java
+++ util/src/main/java/com/github/mcengine/Util.java
@@ -2,6 +2,12 @@ package com.github.mcengine;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Properties;
 
 public class Util {
@@ -19,5 +25,24 @@ public class Util {
 
         // Return the Properties object
         return properties;
+    }
+
+    public static String getGitHubLatestReleaseTag(String organization, String repo) throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.github.com/repos/" + organization + "/" + repo + "/releases/latest"))
+                .header("Accept", "application/vnd.github.v3+json")
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 200) {
+            // Parse JSON response to get the tag name
+            String responseBody = response.body();
+            int tagStartIndex = responseBody.indexOf("\"tag_name\":") + "\"tag_name\":".length();
+            int tagEndIndex = responseBody.indexOf(",", tagStartIndex);
+            return responseBody.substring(tagStartIndex, tagEndIndex).replace("\"", "").trim();
+        } else {
+            throw new IOException("Failed to fetch latest release tag. Status code: " + response.statusCode());
+        }
     }
 }
